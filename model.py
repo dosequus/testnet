27e0f832e1e9a8f36d14a1eece05fd77469d0fb9
dsiher <david.siher@gmail.com>
2022-12-05 17:16:22 -0800

    Python type annotation quotes + leaf fix

--- model.py
+++ model.py
@@ -6,6 +6,7 @@ from chess import Move, Piece, PieceType, Square
 from vit_pytorch import ViT
 import numpy as np
 
+
 class TransformerNet():
     class PositionalEncoding(nn.Module):
         def __init__(self, d_hidden, max_len=64) -> None:
@@ -13,13 +14,14 @@ class TransformerNet():
             pe = torch.arange(max_len).unsqueeze(1)
             self.register_buffer('pe', pe)
 
-        def forward(self, x : torch.Tensor) -> torch.Tensor:
+        def forward(self, x: torch.Tensor) -> torch.Tensor:
             x = x + self.pe[:x.size(0)]
             return x
     """
         ntoken: size of tokens => 12 for chess
         emsize = 
     """
+
     def __init__(self):
         super().__init__()
         self.vit = ViT(
@@ -45,7 +47,7 @@ class TransformerNet():
             nn.Linear(256, 73),
         )
 
-    def __call__(self, x : torch.Tensor, mask : torch.Tensor) -> torch.Tensor:
+    def __call__(self, x: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
         return self.forward(x, mask)
 
     def loss(self, data_true, data_pred):
@@ -53,23 +55,23 @@ class TransformerNet():
         v_pred = data_pred['value']
         return F.mse_loss(v_pred, v_true)
 
-    def predict(self, x : torch.Tensor, mask : torch.Tensor):
+    def predict(self, x: torch.Tensor, mask: torch.Tensor):
         with torch.no_grad():
             return self.forward(x, mask)
 
-    def forward(self, x: torch.Tensor, mask : torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
         x = x.reshape(12, 8, 8).unsqueeze(0)
         print(x.dtype)
         z = self.vit(x)
 
-        z = z.squeeze().reshape(8,8,-1)
+        z = z.squeeze().reshape(8, 8, -1)
         print(z.shape)
         v = self.value_head(z)
         v = v.reshape(-1).sum()
         v = torch.tanh(v)
 
         pi = self.policy_head(z)
-        pi = pi.reshape(8,8,-1)
+        pi = pi.reshape(8, 8, -1)
         pi = F.softmax(pi, dim=2)
         pi = pi * mask
         pi = F.softmax(pi, dim=2)
@@ -77,28 +79,33 @@ class TransformerNet():
         return pi, v
 
     @staticmethod
-    def get_legal_move_mask(moves: list[Move], piece_map: dict[Square, Piece]) -> torch.TensorType:
+    def get_legal_move_mask(moves: "list[Move]", piece_map: "dict[Square, Piece]") -> torch.TensorType:
         # pi[i][j][k] = Pr(positions[i][j] * t(k) | state)
-        KNIGHT_MOVES = [(2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (1, -2), (2, -1)]
+        KNIGHT_MOVES = [(2, 1), (1, 2), (-1, 2), (-2, 1),
+                        (-2, -1), (-1, -2), (1, -2), (2, -1)]
         # QUEEN_MOVES = [N,NE,E,SE,S,SW,W,NW]
-        QUEEN_MOVES = [(0,1), (1,1), (0,1), (-1,1), (0,-1), (-1, -1), (-1,0), (-1,1)]
-        
+        QUEEN_MOVES = [(0, 1), (1, 1), (0, 1), (-1, 1),
+                       (0, -1), (-1, -1), (-1, 0), (-1, 1)]
+
         UNDERPROMOTIONS = (chess.KNIGHT, chess.BISHOP, chess.ROOK)
-        
-        mask = torch.zeros(8, 8, 73) # moves are illegal to start
+
+        mask = torch.zeros(8, 8, 73)  # moves are illegal to start
         for move in moves:
-            from_i, to_i = chess.square_file(move.from_square), chess.square_file(move.to_square)
-            from_j, to_j =  chess.square_rank(move.from_square), chess.square_rank(move.to_square)
-            
-            if move.promotion is not None and move.promotion != chess.QUEEN: # "underpromotions
+            from_i, to_i = chess.square_file(
+                move.from_square), chess.square_file(move.to_square)
+            from_j, to_j = chess.square_rank(
+                move.from_square), chess.square_rank(move.to_square)
+
+            if move.promotion is not None and move.promotion != chess.QUEEN:  # "underpromotions
                 RIGHT = int(to_i > to_j)
-                mask[from_i][from_j][UNDERPROMOTIONS.index(move.promotion)+RIGHT] = 1
-                
-            if piece_map[move.from_square].piece_type == chess.KNIGHT: # knight moves
+                mask[from_i][from_j][UNDERPROMOTIONS.index(
+                    move.promotion)+RIGHT] = 1
+
+            if piece_map[move.from_square].piece_type == chess.KNIGHT:  # knight moves
                 di = to_i - from_i
                 dj = to_j - from_j
 
-                mask[from_i][from_j][56+KNIGHT_MOVES.index((di,dj))] = 1
+                mask[from_i][from_j][56+KNIGHT_MOVES.index((di, dj))] = 1
             if piece_map[move.from_square].piece_type in (chess.PAWN, chess.KING, chess.ROOK, chess.BISHOP, chess.QUEEN):
                 di = to_i - from_i
                 dj = to_j - from_j
@@ -106,32 +113,39 @@ class TransformerNet():
                 di = di//abs(di) if di > 0 else 0
                 dj = dj//abs(dj) if dj > 0 else 0
 
-                mask[from_i][from_j][QUEEN_MOVES.index((di, dj))*7+chess.square_distance(move.from_square, move.to_square)-1] = 1
+                mask[from_i][from_j][QUEEN_MOVES.index(
+                    (di, dj))*7+chess.square_distance(move.from_square, move.to_square)-1] = 1
 
         return mask
-            # if piece_map[move.from_square]
+        # if piece_map[move.from_square]
 
     @staticmethod
-    def pi_to_move_map(pi: torch.Tensor, moves: list[Move], piece_map: dict[Square, Piece]) -> dict[Move, int]:            
-        KNIGHT_MOVES = [(2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (1, -2), (2, -1)]
+    def pi_to_move_map(pi: torch.Tensor, moves: "list[Move]", piece_map: "dict[Square, Piece]") -> "dict[Move, int]":
+        KNIGHT_MOVES = [(2, 1), (1, 2), (-1, 2), (-2, 1),
+                        (-2, -1), (-1, -2), (1, -2), (2, -1)]
         # QUEEN_MOVES = [N,NE,E,SE,S,SW,W,NW]
-        QUEEN_MOVES = [(0,1), (1,1), (0,1), (-1,1), (0,-1), (-1, -1), (-1,0), (-1,1)]
-        
+        QUEEN_MOVES = [(0, 1), (1, 1), (0, 1), (-1, 1),
+                       (0, -1), (-1, -1), (-1, 0), (-1, 1)]
+
         UNDERPROMOTIONS = (chess.KNIGHT, chess.BISHOP, chess.ROOK)
         move_mapping = {}
         for move in moves:
-            from_i, to_i = chess.square_file(move.from_square), chess.square_file(move.to_square)
-            from_j, to_j =  chess.square_rank(move.from_square), chess.square_rank(move.to_square)
-            
-            if move.promotion is not None and move.promotion != chess.QUEEN: # "underpromotions
+            from_i, to_i = chess.square_file(
+                move.from_square), chess.square_file(move.to_square)
+            from_j, to_j = chess.square_rank(
+                move.from_square), chess.square_rank(move.to_square)
+
+            if move.promotion is not None and move.promotion != chess.QUEEN:  # "underpromotions
                 RIGHT = int(to_i > to_j)
-                move_mapping[move] = pi[from_i][from_j][UNDERPROMOTIONS.index(move.promotion)+RIGHT].item()
-                
-            if piece_map[move.from_square].piece_type == chess.KNIGHT: # knight moves
+                move_mapping[move] = pi[from_i][from_j][UNDERPROMOTIONS.index(
+                    move.promotion)+RIGHT].item()
+
+            if piece_map[move.from_square].piece_type == chess.KNIGHT:  # knight moves
                 di = to_i - from_i
                 dj = to_j - from_j
 
-                move_mapping[move] = pi[from_i][from_j][56+KNIGHT_MOVES.index((di,dj))].item()
+                move_mapping[move] = pi[from_i][from_j][56 +
+                                                        KNIGHT_MOVES.index((di, dj))].item()
             if piece_map[move.from_square].piece_type in (chess.PAWN, chess.KING, chess.ROOK, chess.BISHOP, chess.QUEEN):
                 di = to_i - from_i
                 dj = to_j - from_j
@@ -139,11 +153,12 @@ class TransformerNet():
                 di = di//abs(di) if di > 0 else 0
                 dj = dj//abs(dj) if dj > 0 else 0
 
-                move_mapping[move] = pi[from_i][from_j][QUEEN_MOVES.index((di, dj))*7+chess.square_distance(move.from_square, move.to_square)-1].item()
+                move_mapping[move] = pi[from_i][from_j][QUEEN_MOVES.index(
+                    (di, dj))*7+chess.square_distance(move.from_square, move.to_square)-1].item()
         return move_mapping
 
-
     # TODO make sure that works lol
+
     def save_model(self, filepath='checkpoints/best_model'):
         torch.save(self)
 
@@ -151,6 +166,6 @@ class TransformerNet():
     def load_model(filepath):
         model = torch.load(filepath)
         return model
-        
+
     def checkpoint(self):
         return super().checkpoint()
--- search.py
+++ search.py
@@ -6,15 +6,17 @@ from games.chessboard import ChessGame
 
 from model import TransformerNet
 
+
 def score(parent, child):
-    prior = child.prior * math.sqrt( parent.prior / (child.visit_count + 1))
+    prior = child.prior * math.sqrt(parent.prior / (child.visit_count + 1))
 
     if child.visit_count > 0:
         value = -child.value()
-    else: 
+    else:
         value = 0
 
     return value + prior
+
 
 class Node:
     def __init__(self, prior, player) -> None:
@@ -27,16 +29,17 @@ class Node:
 
     @property
     def leaf(self):
-        return len(self.children) > 0
+        return len(self.children) == 0
 
     def value(self):
         if self.visited == 0:
-            return 0 
+            return 0
         else:
             return self.value_sum / self.visited
 
     def select_action(self, temperature):
-        visit_counts = np.array((child.visited for child in self.children.values()))
+        visit_counts = np.array(
+            (child.visited for child in self.children.values()))
         actions = (action for action in self.children.keys())
 
         if temperature == 0:
@@ -45,10 +48,11 @@ class Node:
             action = np.random.choice(actions)
         else:
             visited_distribution = visit_counts ** (1/temperature)
-            visited_distribution = visited_distribution / np.sum(visited_distribution)
+            visited_distribution = visited_distribution / \
+                np.sum(visited_distribution)
             action = np.random.choice(actions, p=visited_distribution)
         return action
-    
+
     def select_child(self):
         best_score = -np.inf
         best_action = -1
@@ -62,32 +66,33 @@ class Node:
                 best_child = child
 
         return best_action, best_child
-    
+
     def expand(self, state, player, pi):
         self.player = player
         self.state = state
         for a, pr in enumerate(pi):
             if pr != 0:
                 self.children[a] = Node(prior=pr, player=(self.player * -1))
-    
+
+
 class MCTS:
-    
-    def __init__(self, game : ChessGame, **kwargs) -> None:
+
+    def __init__(self, game: ChessGame, **kwargs) -> None:
         self.game = game
         self.num_simulations = kwargs.get('num_simulations')
 
-    def best_move(self, model : TransformerNet):
+    def best_move(self, model: TransformerNet):
         root = self.run(self.game, model, self.game.turn)
         return root.select_action(0)
 
-
-    def run(self, game : ChessGame, model: TransformerNet, player, depth=50):
+    def run(self, game: ChessGame, model: TransformerNet, player, depth=50):
         root = Node(0, player)
 
         state = game.state()
         valid_moves = game.valid_moves()
-        pi = np.array(( (mv, math.pow(len(valid_moves), -1)) for mv in valid_moves))
-        
+        pi = np.array(((mv, math.pow(len(valid_moves), -1))
+                       for mv in valid_moves))
+
         root.expand(state, player=player, pi=pi)
 
         for _ in range(self.num_simulations):
@@ -102,21 +107,22 @@ class MCTS:
 
             # we are at a leaf node
             # state = n
-            
+
             # make a move
             next_state = game.next_state(state, a)
             value = game.state_score(player)
 
-            if value is None: 
+            if value is None:
                 pi, value = model.predict(next_state, torch.empty(1))
                 # node.expand(next_state, player=)
 
             # self.backup(stack, value, parent.player * -1)
-            if curr_depth == 0: break
+            if curr_depth == 0:
+                break
 
-        return root 
+        return root
 
-    def backup(self, stack : list[Node], value, player):
+    def backup(self, stack: "list[Node]", value, player):
         while stack:
             node = stack.pop(-1)
             node.value_sum += (value if node.player else -value)
